5a7cc2e2357b08020248bd8c53a48151d7aa94d2
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-12-06 20:47:46 +0600

    Our own custom error messages in Formik

--- src/components/my_formik/index.jsx
+++ src/components/my_formik/index.jsx
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 import {Formik, Form, Field} from 'formik';
 import MyForm from './form';
+import * as Yup from 'yup';
+
+
+const myValidationSchema = Yup.object().shape({
+    name: Yup.string('Must Be String')
+        .required('Name is Required')
+        .min(3, 'Min 3 Chars')
+        .max(10, 'Max 10 Chars'),
+    email: Yup.string()
+        .email('Invalid email address')
+        .required('Email is Required'),
+    password: Yup.string()
+        .min(4,'Min 4 Chars')
+        .required('Password is Required')
+});
 
 //1st way to handle Form in Formik
 class MyFormik extends Component {
@@ -18,30 +33,51 @@ class MyFormik extends Component {
                         console.log(values);
                         console.log(formikBag);
                     }}
+                    validationSchema={myValidationSchema}
+                    // validateOnBlur={true}
+                    // validateOnChange={false}
                 >
 
                     {(props) => {
-                        // console.log(props);
+                        console.log('Errors: ', props.errors);
                         
                         return (
                             <div> {/*ekhane ei div er proyojon nai, karon upore div ache */}
                                 <Form>
-                                    <Field
-                                        name='name'
-                                        placeholder='Enter your name'
-                                    />
+                                    <div>
+                                        <Field
+                                            name='name'
+                                            placeholder='Enter your name'
+                                        />
+                                        {/*Custom error message setup*/}
+                                        {props.errors.name && props.touched.name && (
+                                            <div>{props.errors.name}</div>
+                                        )}
+                                    </div>
                                     <br/>
-                                    <Field
-                                        type='email'
-                                        name='email'
-                                        placeholder='Enter your email'
-                                    />
+                                    <div>
+                                        <Field
+                                            type='email'
+                                            name='email'
+                                            placeholder='Enter your email'
+                                        />
+                                        {/*Custom error message setup*/}
+                                        {props.errors.email && props.touched.email && (
+                                            <div>{props.errors.email}</div>
+                                        )}
+                                    </div>
                                     <br/>
-                                    <Field
-                                        type='password'
-                                        name='password'
-                                        placeholder='Enter your password'
-                                    />
+                                    <div>
+                                        <Field
+                                            type='password'
+                                            name='password'
+                                            placeholder='Enter your password'
+                                        />
+                                        {/*Custom error message setup*/}
+                                        {props.errors.password && props.touched.password && (
+                                            <div>{props.errors.password}</div>
+                                        )}
+                                    </div>
                                     <br/>
                                     <button type='submit'>Submit</button>
                                 </Form>
