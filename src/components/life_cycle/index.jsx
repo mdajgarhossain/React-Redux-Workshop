f3e55c27e062949732d6b70a2ddaf8cf34760e4e
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-12-17 22:23:39 +0600

    API call in componentDidMount()  with fetch() api

--- src/components/life_cycle/index.jsx
+++ src/components/life_cycle/index.jsx
@@ -2,38 +2,53 @@ import React, { Component } from 'react';
 
 import ChildComponent from './child_components';
 
+
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+
 class LifeCycleDemo extends Component {
-    constructor(props) {
-        super(props);
-        // console.log('LCM - constructor(1)');
-    }
-
     state = {
-        allData: ['Data one', 'Data two', 'Data three']
-    };
-
-    handleRemove = (index) => {
-        let newAllData = [...this.state.allData];
-        newAllData.splice(index, 1);
-        this.setState({
-            allData: newAllData
-        });
+        posts: [],
+        error: ''
     }
 
+    componentDidMount() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({posts: data})
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({error: 'Server not found!'});
+            })
+    }
     
+
     render() {
-        
+        const {posts, error} = this.state;
+
+        if(error) {
+            return(
+                <h1>{error}</h1>
+            );
+        }
+
+        if(posts.length === 0) {
+            return(
+                <h1>Loading...</h1>
+            );
+        }
+
         return (
             <div>
-                <h1>Life Cycle Demo:</h1>
-                {this.state.allData.map((data, index) => 
-                    <ChildComponent 
-                        data={data}
-                        key={index}
-                        remove={this.handleRemove}
-                        index={index}
-                    />
-                )}
+                <h1>All posts from fake REST API</h1>
+                {/* <h1>{posts.length === 0 && 'Loading...'}</h1> */}
+                <ul className='list-group'>
+                    {posts.map(post => (
+                        <li key={post.id} className='list-group-item'>{post.title}</li>
+                    ))}
+                </ul>
             </div>
         )
     }
